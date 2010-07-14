2e7f45b882ea6834d194cff0ff1b35e6a98a5383
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2010-07-14 17:40:44 +0200

    fixing example

--- src/example.clj
+++ src/example.clj
@@ -1,5 +1,5 @@
 ;; (c) 2010 Patrick Hochstenbach <patrick.hochstenbach@gmail.com>
-(use 'clj-meercat.marc)
+(use 'clj-marc.parser)
 (def records (parse "data/rug01.export"))
 (def my-seq (marc-seq records))
-(time (doseq [rec my-seq] (println (rec "008" :pos [7 11]))))
+(time (doseq [rec my-seq] (println (rec "245"))))
